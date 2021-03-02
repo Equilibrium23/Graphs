a637b4501ad98c28504e5a354a4acb124dbf7bd5
Aaqwil <62186209+Aaqwil@users.noreply.github.com>
2021-03-02 17:56:12 +0100

    Conversion adjacency matrix <-> list
    Added conversion from adjacency matrix to andjacency list, from adjacency list to adjacency matrix,, function to print graph representation in console and slightly modified reading adjacency list from file.

--- Lab1/graph.py
+++ Lab1/graph.py
@@ -1,33 +1,35 @@
+import numpy as np
+
 class Graph:
     @staticmethod
     def create_graph_representation(representation_type, graph_input):
         if representation_type == "adjacency_list":
-            neighborhood_of_vertexes  = [list(map(int,line.split(". ")[1].split(" "))) for line in graph_input]
-            return Graph(representation_type, {vertex+1:neighborhood_of_vertexes[vertex] for vertex in range(len(neighborhood_of_vertexes))})
+            neighborhood_of_vertexes  = [list(map(int, line.split(". ")[1].split(" "))) for line in graph_input]
+            return Graph(representation_type, {vertex:neighborhood_of_vertexes[vertex] for vertex in range(len(neighborhood_of_vertexes))})
         else:
             # incidence_matrix or adjacency_matrix so graph rep is matrix
-            return Graph(representation_type, [list(map(int,line.split(" "))) for line in graph_input])
+            return Graph(representation_type, [list(map(int, line.split(" "))) for line in graph_input])
 
-    def __init__(self,representation_type, graph_representation):
+    def __init__(self, representation_type, graph_representation):
         self.representation_type = representation_type
         self.graph_representation = graph_representation
     
 
-    def change_graph_representation(self,new_representation_type):
+    def change_graph_representation_to(self, new_representation_type):
         if self.representation_type != new_representation_type:
             if new_representation_type == "adjacency_list":
                 self.change_to_adjacency_list()
                 
-            if new_representation_type == "incidence_matrix":
+            elif new_representation_type == "incidence_matrix":
                 self.change_to_incidence_matrix()
 
-            if new_representation_type == "adjacency_matrix":
+            else:
                 self.change_to_adjacency_matrix()
 
     def change_to_adjacency_matrix(self):
         if self.representation_type == "adjacency_list":
-            pass
-            # to do 
+            self.set_graph_representation_type("adjacency_matrix")
+            self.graph_representation = self.create_adjacency_matrix_from_list()
         if self.representation_type == "incidence_matrix":
             pass
             # to do 
@@ -35,8 +37,8 @@ class Graph:
 
     def change_to_adjacency_list(self):
         if self.representation_type == "adjacency_matrix":
-            pass
-            # to do 
+            self.set_graph_representation_type("adjacency_list")
+            self.graph_representation = self.create_list_from_adjacency_matrix()
         if self.representation_type == "incidence_matrix":
             pass
             # to do 
@@ -49,4 +51,44 @@ class Graph:
             pass
             # to do 
 
-    
+    def create_adjacency_matrix_from_list(self):
+        matrix = np.zeros((len(self.graph_representation), len(self.graph_representation)))
+        matrix = matrix.astype(int)
+        for node in range(len(self.graph_representation)):
+            for neighbor in self.graph_representation[node]:
+                matrix[node][neighbor - 1] = 1
+                matrix[neighbor - 1][node] = 1
+        return matrix
+
+    def create_list_from_adjacency_matrix(self):
+        neighbors = list()
+        row_number = 0
+        for row in self.graph_representation:
+            neighbors.append(list())
+            neighbor = 0
+            for node in row:
+                neighbor = neighbor + 1
+                if node == 1:
+                    neighbors[row_number].append(neighbor)
+            row_number = row_number + 1
+
+        return neighbors
+
+
+    def print_graph_representation(self):
+        print(self.representation_type)
+        if self.representation_type == "adjacency_list":
+            for node in range(len(self.graph_representation)):
+                print(node + 1, end = '. ')
+                for neighbor in self.graph_representation[node]:
+                    print(neighbor, end = ' ')
+                print()
+        else:
+            for row in self.graph_representation:
+                for node in row:
+                    print(node, end = ' ')
+                print()
+        print()
+
+    def set_graph_representation_type(self, new_type):
+        self.representation_type = new_type
--- Lab1/main.py
+++ Lab1/main.py
@@ -2,11 +2,15 @@ from handleInput import check_type_of_input, BadInputException
 from graph import Graph
 
 if __name__ == "__main__":
-    with open ('input/input.txt','r') as graph_input:
+    with open ('input/input.txt', 'r') as graph_input:
         try:
             graph_input = graph_input.readlines()
             input_type = check_type_of_input(graph_input)
-            graph = Graph.create_graph_representation(input_type,graph_input)
-            graph.change_graph_representation("adjacency_matrix")
+            graph = Graph.create_graph_representation(input_type, graph_input)
+            graph.print_graph_representation()
+            graph.change_graph_representation_to("adjacency_matrix")
+            graph.print_graph_representation()
+            graph.change_graph_representation_to("adjacency_list")
+            graph.print_graph_representation()
         except BadInputException:
             print(BadInputException)
