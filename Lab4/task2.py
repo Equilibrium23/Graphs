c1f5fe9f591849f81f7bac1bfe133898a90edc3d
Equilibrium23 <wojciech.jedraski1999@gmail.com>
2021-06-12 18:11:53 +0200

    init lab6

--- Lab4/task2.py
+++ Lab4/task2.py
@@ -2,12 +2,10 @@ from utils.handleInput import parse_graph_input
 from utils.graph import Graph,GraphRepresentationType
 from utils.kosaraju import kosaraju
 from utils.graphRandomizer import generate_Gnp_digraph
+from utils.plot import plot_digraph
 
 if __name__ == "__main__":
-    with open ('input/input2.txt', 'r') as graph_input:
-        digraph_input = graph_input.readlines()
-        parsed_digraph_input = parse_graph_input(GraphRepresentationType.DIGRAF_ADJACENCY_MATRIX, digraph_input)
-        graph = Graph(GraphRepresentationType.DIGRAF_ADJACENCY_MATRIX, parsed_digraph_input)
-        # graph = generate_Gnp_digraph(10,0.2)
-        stronglyConnectedComponents = kosaraju(graph, 0)
-        print(stronglyConnectedComponents)
+    digraph = generate_Gnp_digraph(10,0.2)
+    stronglyConnectedComponents = kosaraju(digraph, 0)
+    print(stronglyConnectedComponents)
+    plot_digraph(di)
