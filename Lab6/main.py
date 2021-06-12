c860080cec83e3f647368caf3a9a3449ee8ee23f
Equilibrium23 <wojciech.jedraski1999@gmail.com>
2021-06-12 19:14:29 +0200

    handle input

---
+++ Lab6/main.py
@@ -0,0 +1,6 @@
+from utils.input import travelling_salesman_graph
+
+if __name__ == "__main__":
+    path1 = 'input/59.dat'
+    path2 = 'input/200.dat'
+    graph = travelling_salesman_graph(path1)
---
+++ Lab6/utils/input.py
@@ -0,0 +1,38 @@
+from math import sqrt
+from .graph import Graph, GraphRepresentationType
+
+class Point:
+    @staticmethod
+    def distance(point1, point2):
+        return sqrt ( pow(point1.x - point2.x, 2) + pow (point1.y - point2.y, 2) )
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    
+def read_input(path: str):
+    with open (path, 'r') as points:
+        graph_points = []
+        for line in points:
+            coordinates = [ float(item) for item in line.split(" ") if item != "" ]
+            graph_points.append( Point( coordinates[0], coordinates[1]) )
+        return graph_points
+
+def travelling_salesman_graph(path):
+    points = read_input(path)
+    adjacency_matrix = []
+    weights_matrix = []
+    for i in range(len(points)):
+        adjacency_matrix.append([])
+        for j in range(len(points)):
+            adjacency_matrix[i].append(1 if i != j else 0)
+    
+    for i in range(len(points)):
+        weights_matrix.append([])
+        for j in range(len(points)):
+            weights_matrix[i].append( Point.distance(points[i], points[j]) )
+    
+    graph = Graph(GraphRepresentationType.ADJACENCY_MATRIX, adjacency_matrix)
+    graph.add_connection_weights(weights_matrix)
+    return graph
